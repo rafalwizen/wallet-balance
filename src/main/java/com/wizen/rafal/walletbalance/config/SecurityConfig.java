cfdb2990c065d005a4a903128d60992567499d75
Rafal Wizen <rafal.wizen@gmail.com>
2023-12-05 21:09:26 +0100

    Add Spring Security and basic temporary authentication

---
+++ src/main/java/com/wizen/rafal/walletbalance/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.wizen.rafal.walletbalance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                                .requestMatchers("/**")
+//                        .hasAnyRole("", "", "")
+                                .permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
