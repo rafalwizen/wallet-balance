83ed58797d09dc8f0768b83d337df428f3bb6b3b
Rafal Wizen <rafal.wizen@gmail.com>
2023-12-11 20:38:29 +0100

    Update db_create.sql

--- db_create.sql
+++ db_create.sql
@@ -1,6 +1,22 @@
 CREATE SCHEMA `wallet_balance_directory`;
 
+CREATE TABLE `wallet_balance_directory`.`wallet` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `user` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`)
+);
+  
+  
 CREATE TABLE `wallet_balance_directory`.`currency` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+  `walletId` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`walletId`) REFERENCES wallet(`id`)
+);
+
+insert into wallet_balance_directory.wallet values (1, 'Rafal', 'Rafal');
+
+insert into wallet_balance_directory.currency values (1, "BTC", 1);
+insert into wallet_balance_directory.currency values (2, "ETH", 1);
